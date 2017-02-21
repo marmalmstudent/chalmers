429893068c16a02240cb033d7dd7b85034aa74e1
marmalmstudent <marmalm@student.chalmers.se>
2017-02-21 21:52:55 +0100

    sort of finished all tasks

--- mcc045/handin4/calcs.py
+++ mcc045/handin4/calcs.py
@@ -5,7 +5,6 @@ This code is adapted for 80 columns
 import sys
 try:
     import numpy as np
-    import numpy.matlib as npm
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
 except ImportError:
@@ -30,60 +29,82 @@ class Handin4():
         """
         Class instantiation
         """
-        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, velocity: %.0f Mm/s, imp: %.0f ohm"
+        self.txtTmpl = " "
         self.taskNbr = None
-        self.figTxt = None
+        self.figTxt = None  # the figure text object
         self.E = None  # E-field
-        self.E_init = None
+        self.E_init = None  # initial E-field
         self.H = None  # H-field
+        self.H_init = None  # initial H-field
         self.dist = None  # Distance traveled
         if (len(np.shape(eps_r)) == 0):
             eps_r = np.array([eps_r], dtype=float)
-        self.eps_r = eps_r
-        self.eps = eps_0*eps_r  # spacial refractive index
+        self.eps_r = eps_r  # relative electic permittivity
+        self.eps = eps_0*eps_r  # electric permittivity
         self.mu = mu_0*mu_r  # magnetic permeability
-        self.EBounds = None  # Boundary condition for E-field
         self.nz = nz  # Number of z-points
         self.nt = nt  # Number of time-steps
-        self.n = np.sqrt(self.eps_r)
+        self.n = np.sqrt(self.eps_r)  # refractive index
         self.lbda_0 = lbda_0  # wavelength in vacuum
-        self.period = self.lbda_0/c_0
-        self.dt = self.period/300
-        self.dz = self.lbda_0/300
-        self.line = None
-        self.line2 = None
+        self.period = self.lbda_0/c_0  # period of the wave [s]
+        self.dt = self.period/300  # time step
+        self.dz = self.lbda_0/300  # spacial step
+        self.line = None  # E-field plot line
+        self.line2 = None  # Poynting field plot line
         self.nItrPlot = 10  # nbr of iterations per plot
-        self.pulseWidth = 10
-        self.pulseOffs = 15
-        self.sigma = 0
-        self.mirrorStartIdx = 1
+        self.pulseWidth = 10  # pulse width in periods
+        self.pulseOffs = 15  # pulse offset in periods
+        self.sigma = np.array([0.0])  # electric conductivity
+        self.mirrBegIdx = 1  # start index (z-direction) of mirror
+        self.mirrEndIdx = 1  # end index (z-direction) of mirror
 
     def setNT(self, newNT):
+        """
+        Sets the number of time steps and adjusts the number of steps
+        to loop between plots to make the program run faster.
+
+        Parameters
+        ----------
+        newNT : int
+            The new numer of time steps
+        """
         self.nt = newNT
         self.nItrPlot = int(newNT/200)
 
     def setNZ(self, newNZ):
+        """
+        Sets the number of spacial steps and adjusts the time steps
+        and the spacial steps to make the program run faster.
+
+        Parameters
+        ----------
+        newNZ : int
+            The new numer of time steps
+        """
         self.nz = newNZ
         self.dt = self.period/(0.015*newNZ)
         self.dz = self.lbda_0/(0.015*newNZ)
 
     def setDZ(self, newDZ):
+        """
+        Sets the spacial step and adjusts the number of spacial steps
+        and the time step to make the program run faster.
+
+        Parameters
+        ----------
+        newDZ : int
+            The new numer of time steps
+        """
         self.dz = newDZ
         self.nz = int(self.lbda_0/(0.015*self.dz))
         self.dt = self.period/(0.015*self.nz)
 
-    def initField(self, envWidth, periodOffs,
-                  peakField, q, m):
+    def initField(self, peakField, q, m):
         """
         Calculates incident field in one position in time step m
 
         Parameters
         ----------
-        envWidth : float
-            width of envelope measured in periods (e.g. =20)
-        periodOffs : float
-            how many periods until the envelop reaches is max value (e.g. =
-            1.5*envWidth)
         peakField : float
             max value of incident field (e.g. =1 V/m)
         q : int
@@ -98,8 +119,8 @@ class Handin4():
             The incident field at time setp m.
         """
         f = 1/self.period
-        envWidth = envWidth*self.period
-        offset = periodOffs*self.period
+        envWidth = self.pulseWidth*self.period
+        offset = self.pulseOffs*self.period
         envelope = peakField*np.exp(-(m*self.dt-offset)**(2*q) /
                                     (envWidth/2)**(2*q))
         # incident field at time step m
@@ -107,6 +128,19 @@ class Handin4():
 
     def applyDielMirr(self, zStartIdx, nHigh, nLow, nPairs):
         """
+        Applies the dielectric mirror to the relative permittivity
+        vector.
+
+        Parameters
+        ----------
+        zStartIdx : int
+            The index in the eps_r vector where the mirror starts
+        nHigh : float
+            The high refractive index
+        nLow : float
+            The low refractive index
+        nPairs : int
+            The number of pairs of log and high refractive index segments
         """
         for i in range(0, nPairs):
             segmLen = int(self.lbda_0/(4*nHigh*self.dz))
@@ -185,11 +219,10 @@ class Handin4():
         if (np.size(self.eps) != 1 and np.size(self.eps) != self.nz):
             print("Size mismatch for E-field and spacial refractive index")
             return
+        # implements the Yee-algorithm
         for i in range(0, self.nt, self.nItrPlot):
             for j in range(0, self.nItrPlot):
-                self.E[0] = self.initField(envWidth=self.pulseWidth,
-                                           periodOffs=self.pulseOffs,
-                                           peakField=1, q=4, m=i+j) /\
+                self.E[0] = self.initField(peakField=1, q=4, m=i+j) /\
                                            self.eps_r[0]
                 self.H += self.diff(self.E)*self.dt/(self.dz*self.mu)
                 self.E[1:self.nz] += (self.dt/self.eps[:self.nz-1]) *\
@@ -216,6 +249,11 @@ class Handin4():
             The text presenting how much time has elapsed.
         """
         if (self.taskNbr == 1):
+            """
+            In task 1 the electric field is plotted along with information
+            about how many iterations have been run, how far the wave has
+            propagated (in ps) and the velocity (in Mm/s)
+            """
             E, iter_idx = simData[0], simData[1]
             self.line.set_data(self.dist[:len(self.H)], E[:len(self.H)])
             dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
@@ -223,46 +261,80 @@ class Handin4():
             self.figTxt.set_text(dst)
             return self.line, self.figTxt
         elif (self.taskNbr == 2):
+            """
+            In task 2 the electric field and the poynting vector is plotted
+            in two separete figures. Also some information about how many
+            iterations that have been run, how far the wave has propagated
+            (in ps) and the wave impedance.
+            """
             E, iter_idx = simData[0], simData[1]
             self.line.set_data(self.dist, E)
             # Note the minus sign
             self.line2.set_data(self.dist[:len(self.H)],
                                 -self.E[:len(self.H)]*self.H)
-            dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
-                                  np.sum(abs(self.E))/np.sum(np.abs(self.H)))
+            waveImp = 0
+            if (np.sum(np.abs(self.H)) != 0):
+                waveImp = np.sum(abs(self.E))/np.sum(np.abs(self.H))
+            dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12, waveImp)
             self.figTxt.set_text(dst)
             return self.line, self.figTxt
         elif (self.taskNbr == 3 or self.taskNbr == 4):
+            """
+            In task 3 and 4 the poynting vector is plotted along with some
+            information about how many iterations that have been run, how far
+            the wave has propagated (in ps), the reflected power and the
+            transmitted power.
+            """
             E, iter_idx = simData[0], simData[1]
             self.line2.set_data(self.dist[:len(self.H)],
                                 -self.E[:len(self.H)]*self.H)
-            rx = np.sum(np.abs(self.E[:len(self.H)]*self.H)[:int(self.nz/4)])
-            tx = np.sum(np.abs(self.E[:len(self.H)]*self.H)[int(self.nz/4):])
+            rx = np.sum(np.abs(self.E[:self.mirrBegIdx] *
+                               self.H[:self.mirrBegIdx]))*self.dz
+            tx = np.sum(np.abs(self.E[self.mirrEndIdx:len(self.H)] *
+                               self.H[self.mirrEndIdx:]))*self.dz
             dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
-                                  rx/(rx+tx*self.n[int(self.nz/4)])*100,
-                                  tx*self.n[int(self.nz/4)] /
-                                  (rx+tx*self.n[int(self.nz/4)])*100)
+                                  rx/(rx+tx*self.n[self.mirrEndIdx])*100,
+                                  tx*self.n[self.mirrEndIdx] /
+                                  (rx+tx*self.n[self.mirrEndIdx])*100)
             self.figTxt.set_text(dst)
             return self.line, self.figTxt
         elif (self.taskNbr == 5):
-            E, iter_idx = simData[0], simData[1]
-            self.line2.set_data(self.dist[:len(self.H)],
-                                -self.E[:len(self.H)]*self.H)
-            rx = np.sum(np.abs(self.E[:len(self.H)]*self.H)[:int(self.nz/4)])
-            tx = np.sum(np.abs(self.E[:len(self.H)]*self.H)[int(self.nz/4):])
-            dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
-                                  rx/(rx+tx*self.n[int(self.nz/4)])*100,
-                                  tx*self.n[int(self.nz/4)] /
-                                  (rx+tx*self.n[int(self.nz/4)])*100)
-            self.figTxt.set_text(dst)
-            return self.line, self.figTxt
-        elif (self.taskNbr == 6):
+            """
+            In task 5 the poynting vector is plotted along with some
+            information about how many iterations that have been run, how far
+            the wave has propagated (in ps), the reflected power and the
+            transmitted power.
+            """
             E, iter_idx = simData[0], simData[1]
             self.line2.set_data(self.dist[:len(self.H)],
                                 -self.E[:len(self.H)]*self.H)
             # these are the amplitudes
-            rx = np.max(np.abs(self.E[:self.mirrorStartIdx]))**2/(2*eta_0)
-            tot = np.max(np.abs(self.E_init))**2/(2*eta_0)
+            rx = np.sum(np.abs(self.E[:self.mirrBegIdx] *
+                               self.H[:self.mirrBegIdx]))*self.dz
+            tx = np.sum(np.abs(self.E[self.mirrEndIdx:len(self.H)] *
+                               self.H[self.mirrEndIdx:]))*self.dz
+            # add 10^-40 to avoid division by zero.
+            tot = np.sum(np.abs(self.E[:len(self.H)]*self.H))*self.dz + 1e-40
+            dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
+                                  rx/tot*100,
+                                  tx/tot*100)
+            self.figTxt.set_text(dst)
+            return self.line, self.figTxt
+        elif (self.taskNbr == 6):
+            """
+            In task 6 the poynting vector is plotted along with some
+            information about how many iterations that have been run, how far
+            the wave has propagated (in ps), the reflected power (as a
+            percentage of the incident power).
+            """
+            E, iter_idx = simData[0], simData[1]
+            self.line2.set_data(self.dist[:len(self.H)],
+                                -self.E[:len(self.H)]*self.H)
+            # these are the amplitudes
+            rx = np.sum(np.abs(self.E[:self.mirrBegIdx] *
+                               self.H[:self.mirrBegIdx]))*self.dz
+            # add 10^-40 to avoid division by zero.
+            tot = np.sum(np.abs(self.E[:len(self.H)]*self.H))*self.dz + 1e-40
             dst = self.txtTmpl % (iter_idx, iter_idx*self.dt*1e12,
                                   rx/tot*100)
             self.figTxt.set_text(dst)
@@ -270,7 +342,8 @@ class Handin4():
 
     def initializeEPlot(self):
         """
-        Initializes the 2D plot of them beam diameter vs iteration number.
+        Initializes the 2D plot of the E-field amplitude vs propagated
+        distance.
         """
         ymax = 2
         self.figTxt = ax1.text(0, 1.1*ymax, "", color="#000000")
@@ -282,7 +355,7 @@ class Handin4():
 
     def initializePPlot(self, dispTxt):
         """
-        Initializes the 2D plot of them beam diameter vs iteration number.
+        Initializes the 2D plot of the poynting vecotr vs propagated distance.
         """
         ymax = 0.003
         if (dispTxt):
@@ -330,42 +403,51 @@ class Handin4():
         """
         Initializes task 3.
         """
-        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f percent, TXPWR: %.1f percent"
+        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f " +\
+                       "percent, TXPWR: %.1f percent"
         self.taskNbr = 3
         self.setNT(12000)
         self.setNZ(20000)
+        self.E_init = self.initField(peakField=1, q=4, m=np.arange(0, self.nt))
+        self.H_init = self.diff(self.E_init)*self.dt/(self.dz*self.mu)
         self.E = np.zeros(self.nz+1)
         self.H = np.zeros(self.nz)
         self.dist = np.linspace(0, self.dz*self.nz,
                                 np.size(self.E, axis=0))*1e6
         self.initializePPlot(True)
         self.eps_r = np.ones(self.nz)
-        self.mirrorStartIdx = int(self.nz/4)
-        self.eps_r[self.mirrorStartIdx:] = 1.5**2
+        self.mirrBegIdx = int(self.nz/4)
+        self.mirrEndIdx = int(self.nz/4)
+        self.eps_r[self.mirrBegIdx:] = 1.5**2
         self.n = np.sqrt(self.eps_r)
         self.eps = self.eps_r*eps_0
 
     def initTask4(self):
         """
         Initializes task 4
+        More samples makes the enegery coservation -> 100 %
         """
-        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f percent, TXPWR: %.1f percent"
+        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f " +\
+                       "percent, TXPWR: %.1f percent"
         self.taskNbr = 4
         self.setNT(12000)
         self.setNZ(20000)
+        self.E_init = self.initField(peakField=1, q=4, m=np.arange(0, self.nt))
+        self.H_init = self.diff(self.E_init)*self.dt/(self.dz*self.mu)
         self.E = np.zeros(self.nz+1)
         self.H = np.zeros(self.nz)
         self.dist = np.linspace(0, self.dz*self.nz,
                                 np.size(self.E, axis=0))*1e6
         self.initializePPlot(True)
-        self.eps_r = np.ones(self.nz)
         n_sub = 1.5
         n_coat = np.sqrt(n_sub)
         nARSampl = self.lbda_0/(4*n_coat*self.dz)
         print(nARSampl)
-        self.mirrorStartIdx = int(self.nz/4)
-        self.eps_r[self.mirrorStartIdx:self.mirrorStartIdx+int(nARSampl)] = n_coat**2
-        self.eps_r[self.mirrorStartIdx+int(nARSampl):] = n_sub**2
+        self.mirrBegIdx = int(self.nz/4)
+        self.mirrEndIdx = self.mirrBegIdx + int(nARSampl)
+        self.eps_r = np.ones(self.nz)
+        self.eps_r[self.mirrBegIdx:self.mirrEndIdx] = n_coat**2
+        self.eps_r[self.mirrEndIdx:] = n_sub**2
         self.n = np.sqrt(self.eps_r)
         self.eps = self.eps_r*eps_0
 
@@ -374,9 +456,9 @@ class Handin4():
         Initializes task 5
         longer pulses yields values closer to them theoretical.
         """
-        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f percent, TXPWR: %.1f percent"
+        self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f " +\
+                       "percent, TXPWR: %.1f percent"
         self.taskNbr = 5
-        # calculate sample distance
         nHigh = 1.7
         nLow = 1.5
         self.pulseWidth = 20
@@ -384,14 +466,16 @@ class Handin4():
         self.calcZSalmpDist(nLow, nHigh, 200, 0.25)
         self.nz = 2*self.nz
         self.setNT(int(self.nz*0.75))
+        self.E_init = self.initField(peakField=1, q=4, m=np.arange(0, self.nt))
+        self.H_init = self.diff(self.E_init)*self.dt/(self.dz*self.mu)
         self.E = np.zeros(self.nz+1)
         self.H = np.zeros(self.nz)
         self.dist = np.linspace(0, self.dz*self.nz,
                                 np.size(self.E, axis=0))*1e6
         self.initializePPlot(True)
         self.eps_r = np.ones(self.nz)
-        self.mirrorStartIdx = int(self.nz/3)
-        self.applyDielMirr(self.mirrorStartIdx, nHigh, nLow, 20)
+        self.mirrBegIdx = int(self.nz/3)
+        self.mirrEndIdx = self.applyDielMirr(self.mirrBegIdx, nHigh, nLow, 20)
         self.n = np.sqrt(self.eps_r)
         self.eps = self.eps_r*eps_0
 
@@ -401,14 +485,13 @@ class Handin4():
         """
         self.txtTmpl = "Iterations: %.0f, Time: %.3f ps, RXPWR: %.1f percent"
         self.taskNbr = 6
-        self.pulseWidth = 20
-        self.pulseOffs = 30
-        self.setNT(3000)
-        self.setNZ(2000)
+        self.pulseWidth = 30
+        self.pulseOffs = 40
+        self.setNT(13000)
+        self.setNZ(8000)
         self.nz = 2*self.nz
-        self.E_init = self.initField(envWidth=self.pulseWidth,
-                                     periodOffs=self.pulseOffs,
-                                     peakField=1, q=4, m=np.arange(0, self.nt))
+        self.E_init = self.initField(peakField=1, q=4, m=np.arange(0, self.nt))
+        self.H_init = self.diff(self.E_init)*self.dt/(self.dz*self.mu)
         self.E = np.zeros(self.nz+1)
         self.H = np.zeros(self.nz)
         self.dist = np.linspace(0, self.dz*self.nz,
@@ -417,9 +500,10 @@ class Handin4():
         self.eps_r = np.ones(self.nz)
         self.sigma = np.zeros(self.nz)
         n_sub = 1.5
-        self.mirrorStartIdx = int(self.nz/3)
-        self.eps_r[self.mirrorStartIdx:] = n_sub**2
-        self.sigma[self.mirrorStartIdx:] = 1e4
+        self.mirrBegIdx = int(self.nz/3)
+        self.mirrEndIdx = int(self.nz/3)
+        self.eps_r[self.mirrBegIdx:] = n_sub**2
+        self.sigma[self.mirrBegIdx:] = -1e2
         self.n = np.sqrt(self.eps_r)
         self.eps = self.eps_r*eps_0
 
