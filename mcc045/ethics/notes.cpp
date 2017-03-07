d8ef596ea8eecbcf525e3be2869182120b7aea96
marmalmstudent <marmalm@student.chalmers.se>
2017-03-07 17:34:38 +0100

    finished the essay and wrote the presentation onte

---
+++ mcc045/ethics/notes.cpp
@@ -0,0 +1,48 @@
+/*
+
+Notes
+----- Free software in enginering
+
+- Free as in freedom. (show the four freedoms if possible)
+
+- Open source-directory focus on *practical benefits*
+
+- Proprietary software is everything else. (that don't have the four freedoms)
+
+--- Why you should care
+
+- Either the users control the program or the program controls the user.
+
+- Who controls the program controls the users. Proprietary software is malware
+
+- Data collection can be profitable and proprietary developers know this.
+
+- Free software is not guaranteed not to be malware.
+
+- We can check the source code to make sure it is not malware.
+
+- Analogy with public contracts.
+
+--- In engineering
+
+- Have you read the EULA of a proprietary program?
+
+- You can not know what a proprietary program does. What you see may not be what you get.
+
+- You do not own the software, only the right to use it.
+
+- What will you do if you don't have the license?
+- What if the company does not exist?
+- What if they abandon the file format.
+
+- You must not publish the code, called private software.
+
+--- Solution
+
+- The software is not the product. you sell a service.
+
+- Every problem is different -- requires different solution.
+
+- Think of how e.g. lawyers make money.
+
+*/
